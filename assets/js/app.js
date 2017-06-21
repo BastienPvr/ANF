e9e12d9222e8c33f20477a45aee693520f8110e3
BastienPvr <bvr.planel@gmail.com>
2017-06-21 16:05:50 +0200

    site vitrine V1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,43 @@ $(document).ready(function(){
     $(".services > div").mouseover(function(){
         this.style.height=("35vw");
         this.style.width=("35vw");
+        this.style.paddingTop=("6.5vw");
+        this.style.fontSize=("40px");
+        this.querySelector("span").style.display=("block");
         // Et on diminue celle de tout les autres
         $(".services > div").not($(this)).each(function(){
             this.style.height=("15vw");
-            this.style.width=("15vw")
+            this.style.width=("15vw");
+            this.style.paddingTop=("6.5vw");
+            this.style.fontSize=("20px")
         })
-    })
+    });
 
     // Au out, on ramène tout les elements à leur taille d'origine
     $(".services > div").mouseout(function(){
         $(".services > div").each(function(){
             this.style.height=("20vw");
-            this.style.width=("20vw")
+            this.style.width=("20vw");
+            this.style.paddingTop=("9vw");
+            this.style.fontSize=("30px");
+            this.querySelector("span").style.display=("none")
         })
+    });
+
+
+    /***********
+    *   FORM   *
+    ***********/
+
+    var theForm = document.getElementById( 'theForm' );
+    new stepsForm( theForm, {
+        onSubmit : function( form ) {
+            // Cache le formulaire
+            classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
+
+            var messageEl = theForm.querySelector( '.final-message' );
+            messageEl.innerHTML = 'Veuillez vérifier les informations renseignées';
+            classie.addClass( messageEl, 'show' )
+        }
     })
 })
